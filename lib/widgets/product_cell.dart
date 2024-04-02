b52c724e5a88ed5e1b5f4a4fc0f946911729c00b
LakmaliKes <62734677+LakmaliKes@users.noreply.github.com>
2024-04-02 14:28:00 +0530

    products list

--- lib/widgets/product_cell.dart
+++ lib/widgets/product_cell.dart
@@ -1,6 +1,7 @@
 import 'package:ecom/models/product_list.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
 class ProductCell extends StatelessWidget {
   final Products product;
@@ -17,9 +18,47 @@ class ProductCell extends StatelessWidget {
         children: [
           Stack(
             children: [
-              ClipRRect(
-                borderRadius: BorderRadius.only(topLeft: Radius.circular(20),topRight: Radius.circular(20)),
-                child: Image.network('https://recipes.net/wp-content/uploads/2021/01/why-eat-real-food.jpg')),
+              Container(
+                padding: EdgeInsets.only(bottom: 12),
+                child: ClipRRect(
+                    borderRadius: BorderRadius.only(topLeft: Radius.circular(20),topRight: Radius.circular(20)),
+                    child: Image.network('https://cdn.mos.cms.futurecdn.net/Ekc54rx2YMgRt5ycD5KYf5.jpg')),
+              ),
+              Positioned(
+                  top: 10,
+                  left: 0,
+                  child: Container(
+                    width: 50,
+                    height: 24,
+                    color: Colors.black,
+                    child:Center(child: Text('${product.discount.toString() ?? ''} %',style: TextStyle(color: Colors.grey),)),
+                  )),
+              Positioned(
+                right: 10,
+                  bottom: 0,
+                  child: Container(
+                    width: 50,
+                height: 24,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.all(Radius.circular(8)),
+                  color: Colors.white,
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.grey.withOpacity(0.5),
+                      spreadRadius: 2,
+                      blurRadius: 5,
+                      offset: Offset(0, 3),
+                    ),
+                  ],
+                ),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Icon(Icons.star,color: Colors.red,size: 15,),
+                        Text((product.rate ?? ''),style: TextStyle(color: Colors.red),),
+                      ],
+                    ),
+              )),
     ]
           ),
           Container(
